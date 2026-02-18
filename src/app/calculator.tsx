838b6ff0794fed29a513460ab00473bd5f87ad0c
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2026-02-18 20:41:28 +0900

    update titles in layout and calculator components for clarity

--- src/app/calculator.tsx
+++ src/app/calculator.tsx
@@ -51,7 +51,7 @@ export default function Calculator() {
   return (
     <Container component="main" fixed>
       <h1>
-        家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算書
+        家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算ツール
       </h1>
 
       <Stack spacing={2} sx={{ mb: 2 }}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title:
-    '家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算書',
+    '家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算ツール',
   description:
-    '家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算書',
+    '家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額を計算するツールです。',
   metadataBase: new URL(
     'https://home-workers-necessary-expenditures.vercel.app',
   ),
