216d92c750c15c187c35fa62a906a067441d67e5
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2026-02-18 16:50:58 +0900

    remove unused SVG and CSS files; simplify layout and page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,4 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-
 import type { Metadata } from 'next';
-import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -24,10 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="ja">
+      <body>{children}</body>
     </html>
   );
 }
