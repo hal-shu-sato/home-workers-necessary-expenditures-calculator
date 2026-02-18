c3011934e5c47571abb4c115965a0a65c7ff2e8a
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2026-02-18 17:56:52 +0900

    update spacing style for Stack component in Calculator

--- src/app/calculator.tsx
+++ src/app/calculator.tsx
@@ -26,7 +26,7 @@ export default function Calculator() {
         家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算書
       </h1>
 
-      <Stack spacing={2} mb="2rem">
+      <Stack spacing={2} sx={{ mb: 2 }}>
         <h2>事業所得</h2>
         <TextField
           id="business-income"
