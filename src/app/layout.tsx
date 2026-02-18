31faeee5eccde25d6c8180d49f614880eee48762
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2026-02-18 20:38:36 +0900

    add metadata for SEO and social sharing in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,20 @@ export const metadata: Metadata = {
     '家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算書',
   description:
     '家内労働者等の事業所得等の所得計算の特例の適用を受ける場合の必要経費の額の計算書',
+  metadataBase: new URL(
+    'https://home-workers-necessary-expenditures.vercel.app',
+  ),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    url: '/',
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary',
+  },
 };
 
 export default function RootLayout({
